9b0f17ce0323da5e3e8604ecd8ff28b6baf48df0
A1sh1teru <reserved.coliseum@gmail.com>
2023-06-30 13:35:36 +0300

    added hamburger nav-menu+grid in players' section

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,38 +1,75 @@
-import React from 'react'
-import logo from "../../img/logo.svg"
-import searchIcon from "../../img/searchIcon.svg"
-import styles from "./Header.module.css"
+import React from "react";
+import logo from "../../img/logo.svg";
+import searchIcon from "../../img/searchIcon.svg";
+import styles from "./Header.module.css";
+import { useState } from "react";
+import cn from "clsx";
 
 const Header = () => {
+  const [isActive, setIsActive] = useState(false)
+
   return (
     <header className={styles.header}>
         <div className={styles.logo}>
-            <a href="/">
-                <img src={logo} alt="logo" className={styles.logo} />
-            </a>
-            
+          <a href="/">
+            <img src={logo} alt="logo" className={styles.logo} />
+          </a>
         </div>
-        <nav className={styles.menu}>
-            <ul>
-                <li className={styles.item}><a href="#">Home</a></li>
-                <li className={styles.item}><a href="#">Fixtures & Results</a></li>
-                <li className={styles.item}><a href="#">Standings</a></li>
-                <li className={styles.item}><a href="#">Players & Staff</a></li>
-                <li className={styles.item}><a href="#">Blog</a></li>
-                <li className={styles.item}><a href="#">FAQ</a></li>
-            </ul>
+        <div
+          className={styles.menuBtn}
+          onClick={() => {
+            if (isActive == true) {
+              setIsActive(false);
+            } else if (isActive == false) {
+              setIsActive(true);
+            }
+          }}
+        >
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+
+        <nav className={cn(styles.menu, { [styles.active]: isActive })}>
+          <ul>
+            <div className={styles.cross} onClick={() => setIsActive(false)}>X</div>
+            <li className={styles.item}>
+              <a href="#">Home</a>
+            </li>
+            <li className={styles.item}>
+              <a href="#">Fixtures & Results</a>
+            </li>
+            <li className={styles.item}>
+              <a href="#">Standings</a>
+            </li>
+            <li className={styles.item}>
+              <a href="#">Players & Staff</a>
+            </li>
+            <li className={styles.item}>
+              <a href="#">Blog</a>
+            </li>
+            <li className={styles.item}>
+              <a href="#">FAQ</a>
+            </li>
+          </ul>
         </nav>
 
-      <form className={styles.form}>
-             <input type="search" name="q" placeholder="Search ..." className={styles.searchPlaceholder} />
-                <button 
-                        type="submit" className={styles.buttonSearch}>
-                </button>
-                <img src={searchIcon} alt="searchIcon" className={styles.searchIcon} />
-      </form>
-
+        <form className={styles.form}>
+          <input
+            type="search"
+            name="q"
+            placeholder="Search ..."
+            className={styles.searchPlaceholder}
+          />
+          <button type="submit" className={styles.buttonSearch}></button>
+          <img
+            src={searchIcon}
+            alt="searchIcon"
+            className={styles.searchIcon}
+          />
+        </form>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
